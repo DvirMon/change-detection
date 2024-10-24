759cc604e2707bcf97be84225ae5acfc33773718
dvir.monajem <dmenajem@gmail.com>
2024-10-24 07:59:01 +0300

    add zone.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TitleCasePipe } from "@angular/common";
+import { Component } from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [ReactiveFormsModule],
+  template: `<h2>{{ title }}</h2>`,
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'change-detection';
+  title = "change-detection";
+
+  control = new FormControl(this.title);
+
+  changeTitle() {
+    this.title = "new title";
+
+    this.control.setValue(this.title);
+  }
 }
