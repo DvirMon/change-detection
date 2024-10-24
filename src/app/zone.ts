e07ff5c2bef44a5f123edf1f94ee25a809cdd6c4
DvirMon <55688722+DvirMon@users.noreply.github.com>
2024-10-24 21:35:09 +0300

    Update zone.ts

--- src/app/zone.ts
+++ src/app/zone.ts
@@ -1,49 +1,14 @@
-export class NgZone {
-  constructor({enableLongStackTrace = false}) {
-   //...
-    forkInnerZoneWithAngularBehavior(self);
-  }
-}
-
 function forkInnerZoneWithAngularBehavior(zone: NgZonePrivate) {
   zone._inner = zone._inner.fork({
     name: 'angular',
-    //...
-}
+    
+    onInvokeTask: (delegate: ZoneDelegate, current: Zone, target: Zone, task: Task, applyThis: any) {},
 
-function onEnter(zone: NgZonePrivate) {
-  zone._nesting++;
-  if (zone.isStable) {
-    zone.isStable = false;
-    zone.onUnstable.emit(null);
-  }
-}
+    onInvoke: (delegate: ZoneDelegate, current: Zone, target: Zone, callback: Function, {},
 
-function onLeave(zone: NgZonePrivate) {
-  zone._nesting--;
-  checkStable(zone);
-}
+    onHasTask:
+        (delegate: ZoneDelegate, current: Zone, target: Zone, hasTaskState: HasTaskState) => {
+        },
 
-/**
- * Provides a noop implementation of `NgZone` which does nothing. This zone requires explicit calls
- * to framework to perform rendering.
- *
- * @internal
- */
-export class NoopNgZone implements NgZone {
-  readonly hasPendingMicrotasks: boolean = false;
-  readonly hasPendingMacrotasks: boolean = false;
-  readonly isStable: boolean = true;
-  readonly onUnstable: EventEmitter<any> = new EventEmitter();
-  readonly onMicrotaskEmpty: EventEmitter<any> = new EventEmitter();
-  readonly onStable: EventEmitter<any> = new EventEmitter();
-  readonly onError: EventEmitter<any> = new EventEmitter();
-
-  run(fn: () => any): any { return fn(); }
-
-  runGuarded(fn: () => any): any { return fn(); }
-
-  runOutsideAngular(fn: () => any): any { return fn(); }
-
-  runTask<T>(fn: () => any): any { return fn(); }
+  });
 }
